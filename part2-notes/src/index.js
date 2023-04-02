a5fca9438b90f1d76c43dd880d5e01f7d521732e
keyxcode <keyxcode@gmail.com>
2023-04-02 22:44:27 +0700

    Update part2-notes packages

--- part2-notes/src/index.js
+++ part2-notes/src/index.js
@@ -1,7 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import axios from "axios";
 import App from "./App";
+
+axios.get("http://localhost:3001/notes").then((res) => {
+  console.log(res);
+
+  const notes = res.data;
+  console.log(notes);
+});
+
+// axios.get("http://localhost:3001/foobar").catch((err) => console.log(err));
 
 const notes = [
   {
