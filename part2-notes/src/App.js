3b4d16d47ca97511899967f657b3bd4b1db9fb52
keyxcode <keyxcode@gmail.com>
2023-04-04 13:05:47 +0700

    Add Notification component to part2-notes

--- part2-notes/src/App.js
+++ part2-notes/src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import Note from "./components/Note";
+import Notification from "./components/Notification";
 import noteService from "./services/notes";
 import "./index.css";
 
@@ -9,6 +10,7 @@ const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
+  const [errorMessage, setErrorMessage] = useState("some error happened...");
 
   useEffect(() => {
     noteService.getAll().then((initialNotes) => {
@@ -57,6 +59,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? "important" : "all"}
