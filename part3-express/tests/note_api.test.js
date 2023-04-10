21d803249e4fe7cec27fef26d7e9d9312bb30729
keyxcode <keyxcode@gmail.com>
2023-04-10 16:03:42 +0700

    Add tests for post route in part3 note app

--- part3-express/tests/note_api.test.js
+++ part3-express/tests/note_api.test.js
@@ -30,7 +30,7 @@ test("notes are returned as json", async () => {
     .get("/api/notes")
     .expect(200)
     .expect("Content-Type", /application\/json/);
-}, 100000);
+});
 
 test("all notes are returned", async () => {
   const response = await api.get("/api/notes");
@@ -45,6 +45,38 @@ test("a specific note is within the returned notes", async () => {
 
   const contents = response.body.map((r) => r.content);
   expect(contents).toContain("Browser can execute only JavaScript");
+});
+
+test("a valid note can be added", async () => {
+  const newNote = {
+    content: "async/await simplifies making async calls",
+    important: true,
+  };
+
+  await api
+    .post("/api/notes")
+    .send(newNote)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+
+  const response = await api.get("/api/notes");
+
+  const contents = response.body.map((r) => r.content);
+
+  expect(response.body).toHaveLength(initialNotes.length + 1);
+  expect(contents).toContain("async/await simplifies making async calls");
+});
+
+test("note without content is not added", async () => {
+  const newNote = {
+    important: true,
+  };
+
+  await api.post("/api/notes").send(newNote).expect(400);
+
+  const response = await api.get("/api/notes");
+
+  expect(response.body).toHaveLength(initialNotes.length);
 });
 
 // afterAll() function of Jest is used to close the connect to the db after the tests
