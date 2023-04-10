67bef3dab0e9edb4fe8d6ea3ccecf294375e6ff8
keyxcode <keyxcode@gmail.com>
2023-04-10 15:36:43 +0700

    Update part3 note app api test

--- part3-express/tests/note_api.test.js
+++ part3-express/tests/note_api.test.js
@@ -3,6 +3,27 @@ const supertest = require("supertest");
 const app = require("../app");
 
 const api = supertest(app);
+const Note = require("../models/note");
+
+const initialNotes = [
+  {
+    content: "HTML is easy",
+    important: false,
+  },
+  {
+    content: "Browser can execute only JavaScript",
+    important: true,
+  },
+];
+
+// beforeEach() is used to initialize the db before each test
+beforeEach(async () => {
+  await Note.deleteMany({});
+  let noteObject = new Note(initialNotes[0]);
+  await noteObject.save();
+  noteObject = new Note(initialNotes[1]);
+  await noteObject.save();
+});
 
 test("notes are returned as json", async () => {
   await api
@@ -11,18 +32,22 @@ test("notes are returned as json", async () => {
     .expect("Content-Type", /application\/json/);
 }, 100000);
 
+test("all notes are returned", async () => {
+  const response = await api.get("/api/notes");
+
+  // note that this expect() method is from jest itself
+  // unlike the .expect() method in the block above which is from supertest
+  expect(response.body).toHaveLength(initialNotes.length);
+});
+
+test("a specific note is within the returned notes", async () => {
+  const response = await api.get("/api/notes");
+
+  const contents = response.body.map((r) => r.content);
+  expect(contents).toContain("Browser can execute only JavaScript");
+});
+
+// afterAll() function of Jest is used to close the connect to the db after the tests
 afterAll(async () => {
   await mongoose.connection.close();
-});
-
-test("there are two notes", async () => {
-  const response = await api.get("/api/notes");
-
-  expect(response.body).toHaveLength(2);
-});
-
-test("the first note is about HTTP methods", async () => {
-  const response = await api.get("/api/notes");
-
-  expect(response.body[0].content).toBe("HTML is easy");
 });
