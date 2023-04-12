a0e49bbefb08697fd2c1ab9595df02d53eb8f5ad
keyxcode <keyxcode@gmail.com>
2023-04-12 17:33:23 +0700

    Use localStorage to remember logged user

--- part3-notes/src/services/notes.js
+++ part3-notes/src/services/notes.js
@@ -11,13 +11,6 @@ const setToken = (newToken) => {
 const getAll = () => {
   const request = axios.get(baseUrl);
 
-  // const nonExisting = {
-  //   id: 10000,
-  //   content: "This note is not saved to server",
-  //   important: true,
-  // };
-  // return request.then((response) => response.data.concat(nonExisting));
-
   return request.then((response) => response.data);
 };
 
@@ -27,11 +20,13 @@ const create = async (newObject) => {
   };
 
   const response = await axios.post(baseUrl, newObject, config);
+
   return response.data;
 };
 
 const update = (id, newObject) => {
   const request = axios.put(`${baseUrl}/${id}`, newObject);
+
   return request.then((response) => response.data);
 };
 
